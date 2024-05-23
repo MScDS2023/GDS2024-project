6d88f6530f71591303e47da1ec912a6ce9647668
Martin Kirkegaard <tjuppen100@gmail.com>
2024-05-23 17:48:23 +0200

    Containerized the other application

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,24 @@
 # Use the official Python image from the Docker Hub
-FROM python:3.9-slim
+FROM python:3.12.3-bookworm
 
 # Set the working directory in the container
 WORKDIR /app
 
+# Install system dependencies
+RUN apt-get update && \
+    apt-get install -y gdal-bin libgdal-dev && \
+    apt-get install -y build-essential
+
+# Set environment variables for GDAL
+ENV CPLUS_INCLUDE_PATH=/usr/include/gdal
+ENV C_INCLUDE_PATH=/usr/include/gdal
+ENV GDAL_VERSION=3.6.2
+ENV GDAL_CONFIG=/usr/bin/gdal-config
+
 # Copy the requirements file into the container
 COPY requirements.txt .
 
-# Install the dependencies
+# Install the Python dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy the rest of the application code into the container
--- flask_app.py
+++ flask_app.py
@@ -1,26 +1,84 @@
-from flask import Flask, render_template
+
+from flask import Flask, request, render_template, render_template_string
+from utils import folium_with_corners
+import os
+
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/something1/')
-def something1():
-    return render_template('dashboard.html')
 
-@app.route('/something2/')
-def something2():
-    return "You are on the Something 2 page."
+@app.route('/create_data/')
+def create_data():
+    return render_template("create_data.html")
 
-@app.route('/something3/')
-def something3():
-    return "You are on the Something 3 page."
 
-@app.route('/something4/')
-def something4():
-    return "You are on the Something 4 page."
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route('/show_data/', methods=['GET', 'POST'])
+def show_data():
+    files_path = "templates/created_data"
+    if request.method == 'POST':
+        selected_file = request.form.get('selected_file')
+        file_path = os.path.join(files_path, selected_file)
+
+        # Check if the file exists and read its content
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as file:
+                file_content = file.read()
+            # Render the content of the file
+            return render_template_string(file_content)
+        else:
+            return "File not found", 404
+
+    # List all files in the specified directory
+    files = [x for x in os.listdir(files_path) if x.endswith(".html")]
+    return render_template('show_data.html', files=files)
+
+
+
+
+
+
+@app.route('/dashboard')
+def dashboard():
+    track_name = request.args.get('track_name')
+    index = request.args.get('index')
+    # Now you can use track_name and index as needed, for example, to render the template
+    return render_template('dashboard.html', track_name=track_name, index=index)
+
+
+
+@app.route('/monza')
+def render_the_map():
+    return render_template('monza.html')
+
+
+
+
+@app.route('/idk', methods=['POST'])
+def display_track():
+    d = dict()
+    year = int(request.form.get('year'))
+    track = request.form.get('track')
+    event_type = request.form.get('event_type')
+        # Get checkbox values
+    d["throttle"] = request.form.get('throttle') == 'throttle'
+    d["speed"] = request.form.get('speed') == 'speed'
+    d["breaking"] = request.form.get('breaking') == 'breaking'
+    d["cluster"] = request.form.get('cluster') == 'cluster'
+
+
+    print(d)
+
+    folium_with_corners(year,track, event_type)
+    html = f"created_data/{year}_{track}_{event_type}.html"
+
+    return render_template(html)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
